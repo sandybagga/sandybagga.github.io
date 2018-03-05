3b789a1845e9472072cf424c2abe20cabab2f82f
Sandy Bagga <sbagga@sfu.ca>
2018-03-05 10:54:55 -0800

    new pages and some more new stuff

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$("#projects").click(function () {
+		// $(".desc-container").removeClass("show-desc");
+		// var descName = $(this).attr("id");
+		// $("#" + descName + "-desc").addClass("show-desc");
+		// event.stopPropagation();
+		$(".container").addClass("add-secondary-container");
+	});
+});
